609fddeee0c22ccd85d5f5e7d41bb662315a37c8
Marco Fras <marco.fras@hotmail.it>
2018-01-11 15:08:29 +0000

    Add basic working structure

--- server.js
+++ server.js
@@ -1,1 +1,15 @@
-console.log("Yo I'm running!")
+"use strict";
+
+const express = require("express");
+const app = express();
+
+app.use(express.static("public"));
+
+app.get("/", (request, response) => {
+   response.sendFile(__dirname + "/views/index.html"); 
+});
+
+let port = process.env.PORT || 3000;
+app.listen(port, function() {
+    console.log("Server listening on port " + port);
+});
